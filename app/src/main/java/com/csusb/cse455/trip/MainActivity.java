5f6be755bb447b7d85b4aa4d94967580fef7ae91
Bogdan Kravtsov <bkboggy@yahoo.com>
2017-04-15 01:04:00 -0700

    Added SMS test code to MainActivity.  Main view now has a button and a phone number input field to test the code.

--- app/src/main/java/com/csusb/cse455/trip/MainActivity.java
+++ app/src/main/java/com/csusb/cse455/trip/MainActivity.java
@@ -1,8 +1,14 @@
 package com.csusb.cse455.trip;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +18,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.jar.*;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -32,6 +41,55 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+    // ---------------------------------------------------------------------------------------------
+    // BEGINNING OF SMS TEST CODE
+    // COMMENT IT ALL OUT IF WORKING ON THIS ACTIVITY
+    // ---------------------------------------------------------------------------------------------
+    public void sendSMS(View view) {
+        SmsManager smsM = SmsManager.getDefault();
+        smsM.sendTextMessage(((EditText)findViewById(R.id.phoneInput)).getText().toString(),
+                null, "Testing my app.", null, null);
+    }
+
+    private static final int TRIP_PERMISSIONS_SEND_SMS = 123;
+
+    public void onSendSMSClick(View view) {
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.SEND_SMS},
+                    TRIP_PERMISSIONS_SEND_SMS);
+        }
+        else
+        {
+            sendSMS(view);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode)
+        {
+            case TRIP_PERMISSIONS_SEND_SMS: {
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    sendSMS(getWindow().getDecorView().getRootView());
+                }
+            }
+            default:
+                return;
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // END OF SMS TEST CODE
+    // ---------------------------------------------------------------------------------------------
+
+
+
 
     @Override
     public void onBackPressed() {
@@ -82,7 +140,10 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_help) {
+        } else if (id == R.id.nav_feedback) {
+
+        } else if (id == R.id.nav_contactus)
+        {
 
         }
 
