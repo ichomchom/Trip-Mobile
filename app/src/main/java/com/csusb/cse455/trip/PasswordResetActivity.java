2179621d6df4c40be76be903b1e25fc9c1046db4
Bogdan Kravtsov <bkboggy@yahoo.com>
2017-04-30 12:47:18 -0700

    Refactored PasswordResetActivity: cleaned up dead code and made it easier to test.

--- app/src/main/java/com/csusb/cse455/trip/PasswordResetActivity.java
+++ app/src/main/java/com/csusb/cse455/trip/PasswordResetActivity.java
@@ -1,20 +1,13 @@
 package com.csusb.cse455.trip;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class PasswordResetActivity extends AppCompatActivity {
@@ -22,83 +15,67 @@ public class PasswordResetActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     @Override
+    // onCreate event handler.
     protected void onCreate(Bundle savedInstanceState) {
+        // Super propagation.
         super.onCreate(savedInstanceState);
+        // Set content view layout.
         setContentView(R.layout.activity_pass_recovery);
 
-        // Create a new Firebase Authentication instance.
+        // Get a new Firebase Authentication instance.
         mAuth = FirebaseAuth.getInstance();
 
         // Get the email entered by a user.
-     //   final TextView email = (TextView) findViewById(R.id.recoveryEmail);
+        final TextView emailView = (TextView) findViewById(R.id.recoveryEmail);
 
-        // On click, try reset.
+        // On click, checks email format.  If valid, attempt resetting.
         findViewById(R.id.recoveryBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              // validate(email.getText().toString());
-                validate();
+                // If format is valid, attempt to reset.
+                if (checkFormat(emailView)) {
+                    tryReset(emailView.getText().toString());
+                    // Finish this Activity.
+                    finish();
+                }
             }
         });
     }
 
     // Validates email format.
-    private void validate() {
-        //Get email Input
-        final TextView email = (TextView) findViewById(R.id.recoveryEmail);
+    private boolean checkFormat(TextView emailView) {
+        // Get the string from view.
+        final String email = emailView.getText().toString();
 
-        View focusView = null;
-        boolean cancel = false;
-
-        //Convert email to string
-        String recoveryEmail = email.getText().toString();
-
-        // Valid email flag.
-        boolean valid = false;
-
-        // Check if Email is valid or not
-        if (!isEmailFormatValid(recoveryEmail)) {
-            email.setError(getString(R.string.invalidEmailFormat));
-            focusView = email;
-            cancel = true;
-        } else {
-            valid = true;
+        // Check if email format is valid.  If not, return false.
+        if (!isEmailFormatValid(email)) {
+            emailView.setError(getString(R.string.invalidEmailFormat));
+            return false;
         }
-
-        // If valid, attempt to reset.
-        if (valid) {
-            tryReset(recoveryEmail);
-        }
-    }
-
-    /*
-    Attempts to reset the password for the provided email address.  We do not check if
-    attempt was successful or failed.  If the user gets the email, they'll know it worked.
-    This is done so that hackers may not attempt to scan through a list of emails to check
-    which ones were successful, meaning they exist.
-    */
-    private void tryReset(String email) {
-        mAuth.sendPasswordResetEmail(email);
-        Toast.makeText(PasswordResetActivity.this,
-                "An email with password reset instructions was sent.",
-                Toast.LENGTH_LONG).show();
-        // Transition to the login screen.
-        transitionToLogin();
-    }
-
-    // Transition to the LoginActivity screen.
-    private void transitionToLogin()
-    {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        // Otherwise, return true.
+        return true;
     }
 
     // Checks if email is formatted properly.
     private boolean isEmailFormatValid(String email) {
+        // First check if it's empty.  Return false if it is.
         if (TextUtils.isEmpty(email)) {
             return false;
         } else {
+            // Otherwise, confirm it matches an accepted email format.  Return the resulting
+            // boolean value.
             return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+    }
+
+    // Attempts to reset the password for the provided email address.  We do not check if
+    // attempt was successful or failed.  If the user gets the email, they'll know it worked.
+    private void tryReset(String email) {
+        if (mAuth != null) {
+            mAuth.sendPasswordResetEmail(email);
+            Toast.makeText(PasswordResetActivity.this,
+                    "An email with password reset instructions was sent.",
+                    Toast.LENGTH_LONG).show();
         }
     }
 }
